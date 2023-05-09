44d65769875ba63e1a3863413021b59dea3dbbc8
rebeccawaweru <wawerur95@gmail.com>
2023-05-09 06:42:36 -0700

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    y = ""
+    for x in str:
+        if ord('a') <= ord(x) <= ord('z'):
+           y += chr(ord(x) - 32)
+        else:
+            y += x
+    print("{}".format(y))
