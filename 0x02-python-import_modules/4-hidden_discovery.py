d914fc1b5a23e3adb2bda972be39e4e52d0fd18a
rebeccawaweru <wawerur95@gmail.com>
2023-05-11 06:45:16 -0700

    hidden module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+if __name__ = '__main__':
+    import hidden_4
+    lst = dir(hidden_4)
+    for name in lst:
+        if not name.startswith('__'):
+            print("{:s}".format(name))
