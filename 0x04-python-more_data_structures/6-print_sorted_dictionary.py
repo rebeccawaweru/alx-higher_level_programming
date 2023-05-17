f9c5932ca7f7438a9f0ea14fb7cc3d657ed1907a
rebeccawaweru <wawerur95@gmail.com>
2023-05-17 04:54:51 -0700

    sort with ordered keys correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,3 +4,13 @@ def print_sorted_dictionary(a_dictionary):
     for key in keys_sorted:
         val = a_dictionary[key]
         print(f"{key}: {val}")
+
+
+if __name__ == '__main__':
+    a_dictionary = {
+            'language': "C",
+            'Number:': 89,
+            'track': "Low level",
+            'ids': [1, 2, 3]
+    }
+    print_sorted_dictionary(a_dictionary)
