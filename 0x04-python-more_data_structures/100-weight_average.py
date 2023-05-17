b22647f8ba2790447ee24b5049fb980fca39b146
rebeccawaweru <wawerur95@gmail.com>
2023-05-17 10:44:56 -0700

    map correction

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         average = 0
-    x = sum(map(lambda x: x[0] * x[1], my_list))
-    y = sum(map(lambda x: x[1], my_list))
-    average = x / y
+    else:
+        x = sum(map(lambda x: x[0] * x[1], my_list))
+        y = sum(map(lambda x: x[1], my_list))
+        average = x / y
     return average
