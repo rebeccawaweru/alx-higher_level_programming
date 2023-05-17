e6bb508da5fe55dfc9055b56cd6156038fa53380
rebeccawaweru <wawerur95@gmail.com>
2023-05-17 11:08:07 -0700

    complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value:
+        for key in list(a_dictionary.keys()):
+            if value == a_dictionary[key]:
+                del a_dictionary[key]
+    return a_dictionary
