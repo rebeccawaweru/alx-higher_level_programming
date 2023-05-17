0152559f40463fcb20616e56b1b25ec8ac8f5fb5
rebeccawaweru <wawerur95@gmail.com>
2023-05-17 06:38:15 -0700

    max integer correction

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is not None:
         keys = a_dictionary.keys()
-        return max(keys, key=lambda key: a_dictionary[key])
+        for x in keys:
+            if a_dictionary[x] is None:
+                return None
+            else:
+                return max(keys, key=lambda key: a_dictionary[key])
