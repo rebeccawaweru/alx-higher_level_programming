0429afa3c868e0842e0c3bd3f24a0d2810088d1c
rebeccawaweru <wawerur95@gmail.com>
2023-05-29 08:00:40 -0700

    bit manipulation update

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     final = 0
-    for j in range(1, 4):
+    for j in range(1, 3):
         try:
             if j > a:
                 raise Exception("Too far")
             else:
-                final += (a ** b) / j
+                final += a ** b / j
         except IndexError:
             final = b + a
             break
