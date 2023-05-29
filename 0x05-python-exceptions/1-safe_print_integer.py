814f16d7675f3ab17742fd8978a2e3c839a5c35e
rebeccawaweru <wawerur95@gmail.com>
2023-05-29 03:46:44 -0700

    print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+        else:
+            return False
+    except ValueError:
+        return False
+    return True
