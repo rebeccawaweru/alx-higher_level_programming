968fed7507bb0dc46264e2fcef3383ce613714a6
rebeccawaweru <wawerur95@gmail.com>
2023-05-30 07:37:19 -0700

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines square by private instance attribute: size"""
+
+
+class Square:
+
+    """A square"""
+    def __init__(self, size):
+        """Initializing a new square
+        Args:
+        size (int): size of the new square
+        """
+        self.__size = size
