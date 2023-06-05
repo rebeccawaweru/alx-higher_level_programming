031c5975846797d502939c7932041451f06fe08b
rebeccawaweru <wawerur95@gmail.com>
2023-06-05 11:13:17 -0700

    instance update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -45,6 +45,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Return the area of the rectangle"""
