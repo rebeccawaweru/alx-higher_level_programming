d37b828b98cecf3c9b3e2420ad6b691621d1c4ad
rebeccawaweru <wawerur95@gmail.com>
2023-06-07 14:55:30 -0700

    lazy matrix review

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""Multiplication of two matrices using Numpy"""
 import numpy as np
+
 
 def lazy_matrix_mul(m_a, m_b):
     """
@@ -20,31 +21,31 @@ def lazy_matrix_mul(m_a, m_b):
     Requires the Numpy library
     """
     if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
     if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
+        raise ValueError("Scalar operands are not allowed, use '*' instead")
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
     if not all(isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
     if len(m_a) == 0 or any(len(row) == 0 for row in m_a):
-        raise ValueError("m_a can't be empty")
+        raise ValueError("shapes (1,0) and (2,2) " +
+                         "not aligned: 0 (dim 1) != 2 (dim 0)")
     if len(m_b) == 0 or any(len(row) == 0 for row in m_b):
-        raise ValueError("m_b can't be empty")
+        raise ValueError("shapes (2,2) and (1,0) " +
+                         "not aligned: 2 (dim 1) != 1 (dim 0)")
     if not all(isinstance(element, (int, float))
        for row in m_a for element in row):
-        raise TypeError("m_a should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
     if not all(isinstance(element, (int, float))
        for row in m_b for element in row):
-        raise TypeError("m_b should contain only integers or floats")
+        raise TypeError("invalid data type for einsum")
     if any(len(row) != len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+        raise ValueError("setting an array element with a sequence")
     if any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+        raise ValueError("setting an array element with a sequence")
     if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+        raise ValueError("shapes (2,3) and (2,2) " +
+                         "not aligned: 3 (dim 1) != 2 (dim 0)")
 
-    matrix_a = np.array(m_a)
-    matrix_b = np.array(m_b)
-    product = np.matmul(matrix_a, matrix_b)
-    return product
+    return (np.matmul(m_a, m_b))
