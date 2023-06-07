a66fece5b474570b4aa6d4c6768dbf4b61cbc80e
rebeccawaweru <wawerur95@gmail.com>
2023-06-07 07:41:50 -0700

    name review

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+""" say_my_name
+Prints the first and last name
+Takes two arguments: first name and last name
+Other type raise TypeError
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    """
+    """say_my_name
     Prints the first and the last name
 
     Arguments:
@@ -11,6 +18,8 @@ def say_my_name(first_name, last_name=""):
     Raises:
     TypeError: if first_name or last_name is not a string
     """
+    if first_name is None and last_name is None:
+        raise TypeError("missing arguments")
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, str):
