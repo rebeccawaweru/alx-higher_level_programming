9bfb50cfdb070c784fe3d319c9b62490be497f90
rebeccawaweru <wawerur95@gmail.com>
2023-06-13 07:47:21 -0700

    add item review

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 filename = "add_item.json"
+my_list = []
 my_list = load_from_json_file(filename)
 for argument in argv[1:]:
     my_list.append(argument)
