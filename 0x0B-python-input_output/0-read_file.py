eb834d9adf1ac304bcaad1e455769726c67661eb
rebeccawaweru <wawerur95@gmail.com>
2023-06-13 05:10:35 -0700

    read file review

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Define function for reading file"""
+
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    """Print contents of file to stdout"""
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end="")
