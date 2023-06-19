5fb8a1ca6c7f2273b2e17a07cd364173deac5f32
rebeccawaweru <wawerur95@gmail.com>
2023-06-19 18:10:06 +0300

    advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Define a new class Base"""
+import csv
 
 
 class Base:
@@ -45,3 +46,55 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance where all attributes are set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            dummy_instance = cls()
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+                dictionaries = cls.from_json_string(json_string)
+                instances = [cls.create(**dictionary) for
+                             dictionary in dictionaries]
+                return instances
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Serialize CSV"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            for obj in list_objs:
+                csv_string = cls.to._csv_string(obj)
+                writer.writerow(csv_string)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Deserialize CSV"""
+        filename = cls.__name.__ + ".csv"
+        try:
+            with open(filename, 'r', newline='') as file:
+                reader = csv.reader(file)
+                instances = []
+                for row in reader:
+                    csv_string = ','.join(row)
+                    instance = cls.from_csv_string(csv_string)
+                    instances.append(instance)
+                return instances
+        except FileNotFoundError:
+            return []
